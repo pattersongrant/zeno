c4639127afc1cf1301097d3888f882312cf0ad14
Grant Patterson <pattersongrant06@gmail.com>
2024-04-18 18:04:56 -0400

    animation

--- zeno/src/app/page.tsx
+++ zeno/src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="place-content-center">
+    <main className="">
       <div className="">
         <Image className="m-10 shadow-[0_20px_50px_rgba(0,_0,_0,_0.7)]"
           src="/z.png"
@@ -12,7 +12,7 @@ export default function Home() {
         />
       </div>
       <div className="">
-        <h1 className="animate-typing font-mono text-9xl text-brown">Modernized News.</h1>
+        <h1 className="animate-pulse m-10 font-mono text-white text-9xl">Modernized News.</h1>
       </div>
     </main>
   );
