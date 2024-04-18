75836cc415ab050312e175096a5e5a24eb5c79e8
Grant Patterson <pattersongrant06@gmail.com>
2024-04-18 18:24:31 -0400

    font scaling and blurb

--- zeno/src/app/page.tsx
+++ zeno/src/app/page.tsx
@@ -11,8 +11,9 @@ export default function Home() {
           height={37}
         />
       </div>
-      <div className="">
-        <h1 className="animate-pulse m-10 font-mono text-white text-9xl">Modernized News.</h1>
+      <div className="text-center">
+        <h1 className="animate-pulse m-10 font-mono text-white sm:text-7xl md:text-8xl lg:text-9xl">Modernized <br></br>News.</h1>
+        <h4 className="m-5 mx-auto max-w-[50%] font-mono">Nobody has time to sift through biased traditional news sources. Zeno quickly provides everything you need in an LLM-powered chatbot format, letting you ask questions and stay up-to-date on things you actually care about.</h4>
       </div>
     </main>
   );
