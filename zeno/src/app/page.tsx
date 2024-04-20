3a7038056baca486221456988c776d19b066a54c
Grant Patterson <pattersongrant06@gmail.com>
2024-04-20 09:57:24 -0400

    responsiveness

--- zeno/src/app/page.tsx
+++ zeno/src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           height={37}
         />
         </a>
-        <h1 className="font-mono my-20">Zeno: We&apos;re &quot;breaking news!&quot; Literally!<br></br> You: clever. -_-</h1>
+        <h1 className="opacity-0 lg:opacity-100 font-mono my-20">Zeno: We&apos;re &quot;breaking news!&quot; Literally!<br></br> You: clever. -_-</h1>
         <div className="absolute top-20 right-16">
         <a href="/waitlist" className="font-mono mx-3 inline rounded p-2 border bg-slate-400	hover:shadow-lg ">Download</a>
         <a href="/waitlist" className="font-mono mx-3 inline rounded p-2 hover:border">Demo</a>
@@ -21,7 +21,7 @@ export default function Home() {
         </div>
       </div>
       <div className="text-center">
-        <h1 className="animate-pulse m-10 font-mono text-white sm:text-7xl md:text-8xl lg:text-9xl">Modernized <br></br>News.</h1>
+        <h1 className="animate-pulse m-10 font-mono text-white text-7xl md:text-8xl lg:text-9xl">Modernized <br></br>News.</h1>
         <h4 className="m-5 mx-auto max-w-[50%] font-mono">Nobody has time to sift through biased traditional news sources. Zeno scans the internet for you and quickly provides everything you need in an LLM-powered chatbot format, letting you ask questions, ask for sources, and stay up-to-date on things you actually care about.</h4>
       </div>
     </main>
